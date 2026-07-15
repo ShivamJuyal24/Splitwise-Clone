5cb4dea3bbba53cec342c1d7bcfdd3b4c1b17791
shivam <juyaljii610@gmail.com>
2026-07-15 18:00:57 +0530

    Add Prisma setup with PostgreSQL integration and user model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,25 @@
 import app from "./app";
 import dotenv from "dotenv";
-
+import prisma from "./lib/prisma";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+async function startServer(){
+    try{
+        await prisma.$connect();
+        process.on("SIGINT", async () => {
+            await prisma.$disconnect();
+            process.exit(0);
+        });
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on port ${PORT}`);
+        })
+    }catch(err){
+        console.log("Error starting the server:", err);
+        process.exit(1);
+    }
+}
+
+startServer();
