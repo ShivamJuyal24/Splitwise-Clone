6bef3b617f8fa06904ffc354b9a4e3bb3d23fafc
shivam <juyaljii610@gmail.com>
2026-07-18 00:31:08 +0530

    Add authentication routes and improve error handling in user registration

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -17,6 +17,13 @@ export const registerUser = async (req: Request, res: Response)=>{
             data: user
         })
     }catch(error){
+        if (error instanceof Error) {
+      return res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+
         return res.status(500).json({
             success: false,
             message: "Internal Server Error"
